3913f66b63305517ef77674b7529ab0cd27689ec
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-03-04 15:31:39 +0000

    programme now functioning / needs polishing by inquirer input acceptance criteria, improved css and output method (See to do list in pseudocode)

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const fse = require("fs-extra")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 const { type } = require("os");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
 const teamArray = [];
 
 function runProgramme() {
     generateManager();
-
-
     function generateManager() {
         inquirer.prompt([
             {
@@ -46,20 +46,97 @@ function runProgramme() {
         });
     };
 
+    function generateEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineers name?"
+            },
+            {
+                type: 'input',
+                name: 'engineerID',
+                message: 'What is the engineers employee ID?'
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'What is the engineers Email?'
+            },
+            {
+                type: 'input',
+                name: 'engineerGithub',
+                message: 'What is the engineers GitHub username?'
+            }
+        ]).then((answer) => {
+            const engineer = new Engineer(answer.engineerName, answer.engineerID, answer.engineerEmail, answer.engineerGithub);
+            teamArray.push(engineer)
+            chooseEmployee();
+        });
+    }
+
+    function generateIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the interns name?"
+            },
+            {
+                type: 'input',
+                name: 'internID',
+                message: 'What is the interns employee ID?'
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: 'What is the interns Email?'
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: 'What is the interns school?'
+            }
+        ]).then((answer) => {
+            const intern = new Intern(answer.internName, answer.internID, answer.internEmail, answer.internSchool);
+            teamArray.push(intern);
+            chooseEmployee();
+        });
+    }
+
     function chooseEmployee() {
-        console.log(teamArray);
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employeeChoice",
+                message: "Which employee type would you like to add?",
+                choices: ["Engineer", "Intern", "I am finished building the team."]
+            },
+        ]).then((answer) => {
+            if (answer.employeeChoice === "Engineer")
+                generateEngineer()
+            else if (answer.employeeChoice === "Intern")
+                generateIntern()
+            else
+                buildHTML()
+        });
     }
 };
+
+function buildHTML() {
+    fse.outputFile('output/team.html', render(teamArray));
+
+}
 
 runProgramme();
 
 
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-// Psuedo --
+// finalise
 
-// 1 - create array for team
-// 2 - use inquirer.prompt() to created questions and answers for Manager - take these anwers and create new manager using prebuilt class
-// 3 - run a function called choose / this function asks user what they want to build . then if user wants enginerr run engineer function . then run choose /
-// if intern run intern function then choose (intern and engineer functions also create objects using subclasses and push this to the team array)
-// 4 - once user selects none we return and run the buildHTML function which creates output directory and writes file there with the data as generateTeam(teamArray)
+// 1 - write code to ensure correct entries are made for inquirer inputs . i.e. no blank entries / numbers when relevant etc. 
+// 2 - ensure file outputs correctly / curently overwrites file if already there, can this be improved and possibly write new file? -
+// also add success and error messages and file writing message
+// 3 - clean up CSS/ maybe write style to the new document itself or write a seperate css file that is loaded
